841f67a027ea796b6535d45b95c69a2fa1e3761c
Fedor Akhmetov <f.akhmetov@utwente.nl>
2023-01-19 09:49:37 +0100

    change project structure

---
+++ __init__.py
@@ -0,0 +1,22 @@
+"""
+elipy: a post-processing tool for ABINIT EPH module
+
+Provides `Eliashberg` class object contaning methods for calculation of energy-dependent Eliashberg function a2F from e-ph matrix elements |g_kq|^2
+
+Usage: 
+1. Create file with input parameters `yourfile.py`, e.g.
+-----------------------------------------------------------
+from elipy import Eliashberg
+
+gkq_input = 'indata\in_GSTORE.nc'
+eeig_input = 'indata\in_EIG.nc'
+pheig_input = 'indata\in_PHFRQ'
+
+a2f_class = Eliashberg(gkq_input, eeig_input, pheig_input)
+a2f_class.compute_a2f()
+-----------------------------------------------------------
+2. Run the caculation in the following way: `mpirun -n 4 python yourfile.py > log`
+3. At the end of calculation you will get netCDF4 file `eliashberg_eew.nc' with a2F(e,e',w)
+"""
+
+from .core.eliashberg import *
